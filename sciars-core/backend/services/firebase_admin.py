a77a9789719331ecac94c37ed2751b0cb71752ca
Rhishabh01 <rhishabh89@gmail.com>
2026-04-27 19:53:16 +0530

    firebase fixes

--- sciars-core/backend/services/firebase_admin.py
+++ sciars-core/backend/services/firebase_admin.py
@@ -1,9 +1,11 @@
+import os
+import json
 import firebase_admin
 from firebase_admin import credentials, firestore
 
-import os
+firebase_key = os.environ.get("FIREBASE_KEY")
 
-cred = credentials.Certificate(os.path.join(os.path.dirname(__file__), "serviceAccountKey.json"))
+cred = credentials.Certificate(json.loads(firebase_key))
 firebase_admin.initialize_app(cred)
 
 db = firestore.client()
